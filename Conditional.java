e21b885ac30c3479597fee31ea8fb44a29acc512
kalyanifating <142479185+kalyanifating@users.noreply.github.com>
2023-08-18 15:39:49 +0530

    Add files via upload
    conditional program

---
+++ Conditional.java
@@ -0,0 +1,11 @@
+public class Conditional{
+    public static void main(String[] arg){
+       int number ; //declaration
+       number=29;//Initialization
+       if(number%2 !=0){
+          System.out.print(number +"is odd number");
+       }else{
+          System.out.print(number +"is even number");
+      }
+}
+}
